957fdaf32531d6363875ded89aded093fe9e13b3
CuteyChoma <2556819091@qq.com>
2020-12-28 13:32:31 +0800

    hash路由改为history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,26 @@
-import {createRouter, createWebHashHistory} from 'vue-router'
-
+// import {createRouter, createWebHashHistory} from 'vue-router' //hash模式
+import {createRouter, createWebHistory} from 'vue-router'; //history 模式
 
 // 在 Vue-router新版本中，需要使用createRouter来创建路由
 export default createRouter({
-  // 指定路由的模式,此处使用的是hash模式
-  history: createWebHashHistory(),
-  // 路由地址
-  routes: [{
-    path: '/todolist',
-    // 必须添加.vue后缀
-    component: () =>import('../views/todo-list.vue')
-  }]
+    // 指定路由的模式,此处使用的是hash模式
+    // history: createWebHashHistory(), //hash模式
+    history: createWebHistory(process.env.BASE_URL), //history 模式
+    // 路由地址
+    routes: [{
+        path: '/todolist',
+        // 必须添加.vue后缀
+        component: () => import('../views/todo-list.vue')
+    }, {
+        path: '/mapguide',
+        component: () => import('../views/mapguide/index.vue')
+    },{
+        path:'/',
+        redirect:'/mapguide'
+    }]
 })
+// export default new Router({
+//   mode: 'history', // 去掉url中的#
+//   scrollBehavior: () => ({ y: 0 }),
+//   routes: constantRoutes
+// })
